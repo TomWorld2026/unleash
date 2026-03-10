38103cc309ead76a3141ae4e98f0825913b77064
Thomas Yang <thomas.yang2@gmail.com>
2026-03-10 11:21:19 +1100

    state s3

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "ydd-terraform-state-bucket"
+    key = "unleash/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "ydd-terraform-lock-table"
+  }
 }
 
 provider "aws" {
